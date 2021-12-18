20ee3f1b3e4af1dc1b75b156eef2ea917adaca79
Sagarreys <95458811+Sagarreys@users.noreply.github.com>
2021-12-17 22:12:57 -0600

    Update video.py

--- src/video.py
+++ src/video.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ## ###############################################
+#
+# video.py
+# Proyecto final : Centro Multimedia (centro de entretenimiento para la reproducción de películas, videos, música, y fotografías).
+#
+# Autores:
+# Martínez Bárcenas Edgar Daniel
+# García Reyes Saúl Michel
+# Rodríguez Hernández Alejandro
+# License: MIT
+#
+# ## ###############################################
+
 import sys
 import os.path
 import vlc
